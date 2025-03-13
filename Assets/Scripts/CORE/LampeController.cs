7f540387d456f2e970a0ae54890c1191d3297b50
SP-MMIBdx <stephen.pedder@mmibordeaux.com>
2025-03-13 18:37:04 +0100

    Flashlight functionality (always on)

---
+++ Assets/Scripts/CORE/LampeController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LampeController : MonoBehaviour
+
+{
+public Light LampeLumiere;
+
+/*   {
+        if (Input.GetMouseButtonDown(0)) 
+        {
+            ToggleLampeLumiere();
+        }
+*/   
+}
+
