57477062eadd568ec170cfe306dff87891f94c25
SP-MMIBdx <stephen.pedder@mmibordeaux.com>
2025-03-19 12:03:01 +0100

    Door To Level 3 Functionality

--- Assets/Scripts/LevelElements/DoorToLevel3.cs
+++ Assets/Scripts/LevelElements/DoorToLevel3.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DoorToLevel3 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	[SerializeField] private GameObject doorlvl3;
+	
+	private bool active = true;
+    //Si on touche son collider
+	void OnTriggerEnter(Collider col){
+    	if (col.gameObject.tag == "Player" && active){
+        	HudManager hud = HudManager.instance; //On récupère le hud
+			
+			//Si le joueur a la clé dans son inventaire
+			if(hud.gotItem(Item.ClassicKey)){
+				hud.deleteItem();
+				hud.showTimedMessage("Porte ouverte.");
+				Destroy(doorlvl3);
+				active = false; //Evite de revenir dans le script une fois l'objet supprimé
+				AudioManager am = AudioManager.instance;
+				am.PlaySFX(am.sfx_list.sfx_lock);
+                SceneManager.LoadScene("Level3");
+			} else{
+				hud.showMessage("Vous n'avez pas la clé.");
+			}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    	}
+	}
+	
+	//Si on sort du collider
+	void OnTriggerExit(Collider col){
+		if (col.gameObject.tag == "Player" && active){
+			HudManager hud = HudManager.instance;
+			hud.eraseMessage();
+		}
+	}
 }
